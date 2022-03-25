21523e9820435c21cf274cfaa8469507ae3d3d0e
Tom Lacoma <lacoma.tom@gmail.com>
2022-03-25 13:26:58 +0100

    Created base files

---
+++ client.cpp
@@ -0,0 +1,50 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <iostream>
+#include "base.h"
+
+int main (int argc, char * argv[])
+
+{
+        struct sockaddr_in saddr;
+        struct hostent * server;
+        int s, ret;
+	char buf[256];
+
+	if (argc == 1) {
+		std::cerr << "usage: " << argv[0]
+			  << " [ adresse IP/nom du serveur ]" << std::endl;
+		return 0;
+	}
+
+        s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (s < 0) {
+                std::cerr << "cannot create socket" << std::endl;
+                return 0;
+        }
+
+        saddr.sin_family = AF_INET;
+        saddr.sin_port = htons(DEFAULT_PORT);
+        server = gethostbyname(argv[1]);
+        if (server == NULL) {
+                std::cerr << "gethostbyname() failed" << std::endl;
+		close(s);
+                return 0;
+        }
+        else {
+                memcpy(&saddr.sin_addr, server->h_addr, 4);
+        }
+
+        ret = connect(s, (struct sockaddr *)&saddr, sizeof(saddr));
+        if (ret < 0) {
+                std::cerr << "cannot connect to remote server" << std::endl;
+                close(s);
+                return 0;
+        }
+
+	read(s, buf, sizeof(buf));
+	std::cout << buf << std::endl;
+
+	close(s);
+	return 0;
+}
